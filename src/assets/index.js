3fcca56ac1e0e2e9144509721b792cf7b705c309
Breno Lopes <breno.souza.developer@gmail.com>
2020-01-04 19:06:18 -0300

    criado page do Feed estatica

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,9 @@
-import camera from "../assets/camera.svg";
 import collect from "../assets/collect.svg";
+import camera from "../assets/camera.svg";
+import comment from "../assets/comment.svg";
 import like from "../assets/like.svg";
 import logo from "../assets/logo.svg";
 import more from "../assets/more.svg";
 import send from "../assets/send.svg";
 
-export { camera, collect, like, logo, more, send };
+export { camera, collect, like, logo, more, send, comment };
--- src/pages/FeedUser/index.js
+++ src/pages/FeedUser/index.js
@@ -1,11 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./FeedUser.css";
+import { more, send, comment, like } from "../../assets";
 
 export default class FeedUser extends Component {
   render() {
     return (
-      <div>
-        <h1>Feed</h1>
-      </div>
-    )
+      <section id="post__list">
+        <article>
+          <header>
+            <div className="user__info">
+              <span>Breno Lopes</span>
+              <span className="place">Belo Horizonte</span>
+            </div>
+          <img src={more} alt="mais" />
+          </header>
+          <img src="http://localhost:3333/files/pasto.jpg" alt=""/>
+          <footer>
+            <div className="actions">
+              <img src={like} alt="curtir"/>
+              <img src={comment} alt="comentarios"/>
+              <img src={send} alt="envios"/>
+            </div>
+            <strong>900 curtidas</strong>
+            <p>
+              Um post muito massa da OmniStack!
+              <span>#React #Node #MongoDB</span>
+            </p>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <div className="user__info">
+              <span>Breno Lopes</span>
+              <span className="place">Belo Horizonte</span>
+            </div>
+          <img src={more} alt="mais" />
+          </header>
+          <img src="http://localhost:3333/files/pasto.jpg" alt=""/>
+          <footer>
+            <div className="actions">
+              <img src={like} alt="curtir"/>
+              <img src={comment} alt="comentarios"/>
+              <img src={send} alt="envios"/>
+            </div>
+            <strong>900 curtidas</strong>
+            <p>
+              Um post muito massa da OmniStack!
+              <span>#React #Node #MongoDB</span>
+            </p>
+          </footer>
+        </article>
+      </section>
+    );
   }
 }

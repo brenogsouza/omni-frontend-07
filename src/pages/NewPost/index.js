c7b4f183c3048ab38d8d5fb5256705f016ee9f7b
Breno Lopes <breno.souza.developer@gmail.com>
2020-01-05 23:09:53 -0300

    formulario de criaçao de posts

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 
+import "./NewPost.css"
+
 export default class NewPost extends Component {
   render() {
     return (
-      <div>
-        <h1>New</h1>
-      </div>
+      <form id="new__post">
+        <input type="file"/>
+        <input 
+          type="text" 
+          name="author"
+          placeholder="Autor do post"
+        />
+        <input 
+          type="text" 
+          name="place"
+          placeholder="Local do Post"
+        />
+        <input 
+          type="text" 
+          name="description"
+          placeholder="Descrição do Post"
+        />
+        <input 
+          type="text" 
+          name="hashtags"
+          placeholder="Hashtags do Post"
+        />
+        <button type="submit">Enviar Post</button>
+      </form>
     )
   }
 }

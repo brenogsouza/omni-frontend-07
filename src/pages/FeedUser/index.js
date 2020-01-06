fd02ae97701f4ecc4f65099eb10541942f28a41e
Breno Lopes <breno.souza.developer@gmail.com>
2020-01-05 22:59:40 -0300

    carregando dados da api

--- src/pages/FeedUser/index.js
+++ src/pages/FeedUser/index.js
@@ -1,55 +1,47 @@
 import React, { Component } from "react";
+
+import api from "../../services/api";
+
 import "./FeedUser.css";
 import { more, send, comment, like } from "../../assets";
 
 export default class FeedUser extends Component {
+  state = {
+    feed: []
+  };
+  async componentDidMount() {
+    const response = await api.get("posts");
+
+    this.setState({ feed: response.data });
+  }
+
   render() {
     return (
       <section id="post__list">
-        <article>
-          <header>
-            <div className="user__info">
-              <span>Breno Lopes</span>
-              <span className="place">Belo Horizonte</span>
-            </div>
-          <img src={more} alt="mais" />
-          </header>
-          <img src="http://localhost:3333/files/pasto.jpg" alt=""/>
-          <footer>
-            <div className="actions">
-              <img src={like} alt="curtir"/>
-              <img src={comment} alt="comentarios"/>
-              <img src={send} alt="envios"/>
-            </div>
-            <strong>900 curtidas</strong>
-            <p>
-              Um post muito massa da OmniStack!
-              <span>#React #Node #MongoDB</span>
-            </p>
-          </footer>
-        </article>
-        <article>
-          <header>
-            <div className="user__info">
-              <span>Breno Lopes</span>
-              <span className="place">Belo Horizonte</span>
-            </div>
-          <img src={more} alt="mais" />
-          </header>
-          <img src="http://localhost:3333/files/pasto.jpg" alt=""/>
-          <footer>
-            <div className="actions">
-              <img src={like} alt="curtir"/>
-              <img src={comment} alt="comentarios"/>
-              <img src={send} alt="envios"/>
-            </div>
-            <strong>900 curtidas</strong>
-            <p>
-              Um post muito massa da OmniStack!
-              <span>#React #Node #MongoDB</span>
-            </p>
-          </footer>
-        </article>
+        {this.state.feed.map(post => (
+          <article>
+            <header>
+              <div className="user__info">
+                <span>{post.author}</span>
+        <span className="place">{post.place}</span>
+              </div>
+              <img src={more} alt="mais" />
+            </header>
+            <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+            <footer>
+              <div className="actions">
+                <img src={like} alt="curtir" />
+                <img src={comment} alt="comentarios" />
+                <img src={send} alt="envios" />
+              </div>
+        <strong>{post.likes}</strong>
+              <p>
+                {post.description}
+        <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     );
   }
